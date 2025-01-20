4ac8227097aa1ce4d6160bdd0564d55390daec72
Yanis <156795279+Mezreah@users.noreply.github.com>
2025-01-20 19:40:24 +0100

    Add files via upload

--- Portefolio/View/album.php
+++ Portefolio/View/album.php
@@ -1,1 +1,33 @@
 <?php include "../View/header.php" ?>
+<link rel="stylesheet" href="../CSS/album.css">
+
+<main>
+
+<div class="carousel">
+    <input type="radio" name="slider" id="item-1" checked>
+    <input type="radio" name="slider" id="item-2">
+    <input type="radio" name="slider" id="item-3">
+    
+    <div class="cards">
+        <label class="card" for="item-1" id="song-1">
+            <img src="../images/princesse1.jpeg" alt="Image 1">
+        </label>
+        <label class="card" for="item-2" id="song-2">
+            <img src="../images/princesse2.jpeg" alt="Image 2">
+        </label>
+        <label class="card" for="item-3" id="song-3">
+            <img src="../images/Yanis.JPG" alt="Image 3">
+        </label>
+    </div>
+</div>
+
+<div class="navigation">
+    <label for="item-1" class="bar"></label>
+    <label for="item-2" class="bar"></label>
+    <label for="item-3" class="bar"></label>
+</div>
+
+
+</main>
+
+<?php include "../View/footer.php" ?>
--- Portefolio/View/presentation.php
+++ Portefolio/View/presentation.php
@@ -1,3 +1,89 @@
 <?php include "../View/header.php" ?>
 
-<h1>Au revoir</h1>
+<link rel="stylesheet" href="../CSS/presentation.css">
+
+    <main>
+
+        <section id = "presentation">
+            <article class = "whoAmI">
+                    <h2> Qui suis-je ? </h2>
+                <div id = "text"> 
+                    <p>
+                        Bonjour à toutes et à tous. Je me présente, je m'appelle Yanis MEZIANE et je suis actuellement
+                        en deuxième année de Bachelor en Ingénierie et Numérique au sein de l'EFREI. Passionné depuis quelques années par l'Art avec notamment le Cinéma et le théâtre, j'ai eu l'opportunité de partager mes ambitions et mes passions avec de nombreux étudiant.e.s à travers des activités proposées par des associations étudiantes de l'EFREI. Par mon investissment, j'ai pu rejoindre l'assocation de cinéma en tant que Secrétaire puis en tant Président.
+                    </p>
+            <br>
+                <p>
+                     De nombreuses activités telles que des sorties au cinéma, des séances hebdomadaires au sein des locaux de l'école, ont été organisées dans le but de promouvoir cet Art  En ce qui concerne le théâtre,je suis actuellement professeur d'improvisation et trésorier. Notre but étant de non seulement permettre aux étudiant.e.s de gagner confiance en eux mais également permettre la réalisation d'un spectacle de fin d'année. 
+                    </p>
+                </div>
+            </article>
+        </section>
+
+        <section>
+            <article id = "parcours">
+                <h2>Mon parcours</h2>
+
+                <p class = " textParcours">
+                    <b>
+                        Septembre 2023- Juin 2024
+                    </b>
+                </p>
+
+                <p class = " textParcours">
+                    B1 Développement Web et Application
+                </p>
+                
+                <p class = " textParcours">
+                    <b>
+                        Avril 2024- Juin 2024
+                    </b>
+                </p>
+
+                <p class = " textParcours">
+                    Stage au sein de la DSI de l'Ecole Estienne pour une première approche du travail dans le monde du numérique. Approfondissement des notions apprises en cours ainsi que la découverte de de langages informatiques tels que PHP et l'utilisation de node.
+                </p>
+
+                <p class = " textParcours">
+                    <b>
+                        Septembre 2024- Juin 2025
+                    </b>
+                </p>
+
+                <p class = " textParcours">
+                    B2 Ingénierie et Numérique
+                </p>
+
+
+                <p class = " textParcours">
+                    <b>
+                        Avril 2025 - Juin 2025
+                    </b>
+                </p>
+
+                <p class = " textParcours">
+                        Echange universitaire en Estonie d'une durée de 6 semaines dans le but d'optimiser la pratique de l'anglais et la découverte d'une nouvelle culture.
+                </p>
+            </article>
+        </section>
+
+    <section>
+        <article id = "passion">
+                <h2>Mes passions</h2>
+
+            <div id = "passionBlock">
+                <p>Photographie</p>
+                <p>L'ultimate</p>
+                <p>Le théâtre</p>
+                <p>Le cinéma</p>
+            </div>
+        </article>
+    </section>
+
+
+    </main>
+
+
+
+
+<?php include "../View/footer.php" ?>
